6718cd374db5210005fc313939279419a877b45f
Harel Lustiger <HLustiger@tonkintaylor.co.nz>
2025-11-24 12:01:10 +1300

    refactor: Clean up imports and remove unused dependencies from DESCRIPTION and NAMESPACE

--- R/whitelabel-package.R
+++ R/whitelabel-package.R
@@ -2,12 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom bslib card card_body card_header
-#' @importFrom fresh adminlte_color adminlte_global create_theme use_theme
-#' @importFrom golem activate_js add_resource_path bundle_resources favicon with_golem_options
-#' @importFrom shinydashboard dashboardBody menuItem menuSubItem sidebarMenu tabItem valueBoxOutput renderValueBox valueBox tabItems
-#' @importFrom shinydashboardPlus box dashboardControlbar dashboardFooter dashboardHeader dashboardPage dashboardSidebar controlbarItem controlbarMenu
-#' @importFrom shiny actionButton br checkboxInput column conditionalPanel div fluidRow h2 h5 HTML icon moduleServer NS observeEvent p plotOutput reactive reactiveVal renderPlot req selectInput shinyApp showNotification sliderInput tagList tags testServer updateSliderInput
+#' @importFrom fresh adminlte_color adminlte_global create_theme
+#' @importFrom golem add_resource_path bundle_resources favicon with_golem_options
+#' @importFrom shiny fluidPage moduleServer NS plotOutput renderPlot shinyApp sidebarLayout sliderInput tagList
 ## usethis namespace: end
 NULL
 
